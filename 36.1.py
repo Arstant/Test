8b907b03707f1a2cb4de607a5be1961049c5a392
Arstant <anthony.nicholas.tedesco@gmai.com>
2017-04-25 16:31:24 -0400

    Test Commit 2

--- 36.1.py
+++ 36.1.py
@@ -3,33 +3,67 @@ from sys import exit
 def living_room():
     print "You are greeted by a colorful display of lights cast on an elephant behind the television.\nThere is a pile of coats on the [couch] to the left of the rooms.\nThere is a stairway leading [down] and [up]\nThere also is an ominous looking [box spring] in the corner of the kitchen.\nWhat do you do?"
     choice =raw_input("> ")
+    
     if "down" in choice:
         print "You walk downstairs, passing fredrick the spider"
-        basment()
+        basement()
+    
     elif "up" in choice:
         print "You head to the stairs upwards, 8, 2, 6."
         stair_way()
+    
     elif "couch" in choice:
         dead("The coats on the couch start to stir.\nYou get wrapped up and sucked into a black void.\nSurrounded by tiny gnomes.")
+    
     elif "box" in choice:
         dead("You move to inspect the box spring, when suddenly it sprouts a giant dck and attempts to rape you in the eyes.\nYou narrowly escape back to the frozen suburban nightmare.")
+    
     elif "smell" in choice:
         print "You smell a faint vanilla, upon further inspection you see a glade plug in half burnt out."
+    
     else:
         living_room()
 
 def stair_way():
     print "You arrive at a T hallway with 3 doors.\nThere are two doors to the [right] and one to the [left].\nThere is also a stair way leading [up]."
+    key = False
+    while True:
+        choice = raw_input("> ")
+        
+        if choice == "right" and key:
+            print "You turn to right, there are two doors one is open and contains a toilet and a ninja turtle shower, you go to open the door straight ahead."
+            basement()
+       
+        elif choice == "left" and key:
+            print "You go to the right an ominous squeeky sound is just past the door, you turn the handle..."
+            basement()
+        
+        elif "up" in choice:
+            print "You go upstairs "
+        
+        elif choice == "left" and not key:
+            print "You approach the door but it is locked.\nYou get a slight waft of some kind of baked good"
+
+        elif choice == "right" and not key:
+            print "You approach the door but the sound of furious fapping scares you away"
+        
+        elif choice == "smell" and not key:
+            print "You smell something sweet, you look up and there is a floating key in the center of a doughnut\nYou take the key and can continue"
+            key = True
+        
+        else:
+            print "Yo, you cray cray"
+def basement():
+    print "You walk down the stairs to a fairly open unfinished basement with a large black carpeted section to your left and a washer and dryer to your right.\nIt also looks like some kind of tiny human stores her junk down here.\nThere is also a [tiny] hole in the wall just big enough for your hand to fit in\nIf that scares you, you can always go back upstairs"
     choice = raw_input("> ")
-    if "right" in choice:
-        print "You turn to right, there are two doors one is open and contains a toilet and a ninja turtle shower, you go to open the door straight ahead."
-        dan_room()
-    elif "left"in choice:
-        print "You go to the right an ominous squeeky sound is just past the door, you turn the handle..."
-        alex_room()
-    elif "up" in choice:
-        print "You go upstairs "
-#def basement():
+    if "smell" in choice:
+        print "You smell something musty, seems to be originating from the tiny hole"
+    elif "tiny" in choice:
+        dead("You feel somthing slimy, and realize that you just stuck your hand into a sleeping dragons nostril.\nHe wakes up and says \"Fucking millennials\" \nThen eats your face")
+    elif "back" in choice:
+        living_room()
+    else:
+        basement()
 
 #def kitchen():
 
@@ -70,6 +104,6 @@ def start():
         dead("Your suburban nightmare awaits!")
 
 def dead(why):
-    print why, "Beware of dragons!"
+    print why, "Congratulations!"
     exit(0)
 start()
